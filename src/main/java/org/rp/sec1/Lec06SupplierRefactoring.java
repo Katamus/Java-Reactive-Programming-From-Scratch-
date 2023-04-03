6e71f0730c29009e89b74f88c9653c29bfbd77cb
Christian <christiancatamuscay@r-fast.com>
2023-04-03 16:57:32 -0500

    22.[Mono] - Pipeline Build vs Execution (Mono)

---
+++ src/main/java/org/rp/sec1/Lec06SupplierRefactoring.java
@@ -0,0 +1,23 @@
+package org.rp.sec1;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec06SupplierRefactoring {
+
+    public static void main(String[] args) {
+        getName();
+        getName().subscribe(Util.onNext());
+        getName();
+    }
+
+    private static Mono<String> getName(){
+        System.out.println("entered getName method");
+        return Mono.fromSupplier(()-> {
+            System.out.println("Generating name ...");
+            Util.sleepSeconds(3);
+            return Util.faker().name().fullName();
+        });
+    }
+
+}
