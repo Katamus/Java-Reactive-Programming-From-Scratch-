e765b4fa2ae03588ab9c3de31b4097d745a77191
Christian <christiancatamuscay@r-fast.com>
2023-04-03 16:32:31 -0500

    [Mono] - Emitting Empty / Error (Mono)

---
+++ src/main/java/org/rp/sec1/Lec04MonoEmptyOrError.java
@@ -0,0 +1,25 @@
+package org.rp.sec1;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Mono;
+
+public class Lec04MonoEmptyOrError {
+
+    private static Mono<String> userRepository(int userId){
+        // 1
+        if(userId == 1){
+            return Mono.just(Util.faker().name().firstName());
+        }else if(userId == 2){
+            return Mono.empty();
+        }else
+            return Mono.error(new RuntimeException("Not in the allowed range"));
+    }
+    public static void main(String[] args) {
+        userRepository(20)
+                .subscribe(
+                        Util.onNext(),
+                        Util.onError(),
+                        Util.onComplete()
+                );
+    }
+}
