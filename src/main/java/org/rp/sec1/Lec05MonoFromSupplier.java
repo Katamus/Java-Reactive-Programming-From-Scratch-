34568b9698d50161dbbbdde036f71436ed2ac83b
Christian <christiancatamuscay@r-fast.com>
2023-04-03 16:46:26 -0500

    21. [Mono] - From Callable (Mono)

--- src/main/java/org/rp/sec1/Lec05MonoFromSupplier.java
+++ src/main/java/org/rp/sec1/Lec05MonoFromSupplier.java
@@ -3,13 +3,21 @@ package org.rp.sec1;
 import org.rp.courseutil.Util;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
+
 public class Lec05MonoFromSupplier {
     public static void main(String[] args) {
         // use just only when you have date already
         //Mono<String> mono = Mono.just(getName());
 
-        Mono<String> mono = Mono.fromSupplier(() -> getName());
+        Supplier<String> stringSupplier = () -> getName();
+        Mono<String> mono = Mono.fromSupplier(stringSupplier);
         mono.subscribe(Util.onNext());
+
+        Callable<String> stringCallable = () -> getName();
+        Mono.fromCallable(stringCallable)
+                .subscribe(Util.onNext());
 
     }
 
