02e3c1dce7fee17169eb0202f2b7c12b89f83d70
Christian <christiancatamuscay@r-fast.com>
2023-04-05 16:06:23 -0500

    42.[Flux] - from Mono / Publisher (Flux)

---
+++ src/main/java/org/rp/sec02/helper/Lec09FluxFromMono.java
@@ -0,0 +1,17 @@
+package org.rp.sec02.helper;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Lec09FluxFromMono {
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("a");
+        Flux<String> flux = Flux.from(mono);
+        flux.subscribe(Util.onNext());
+    }
+
+    private static void doSomething(Flux<String> flux){
+
+    }
+}
