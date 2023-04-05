25903a7fe6e404256684cd8b004b1679ae0a3668
Christian <christiancatamuscay@r-fast.com>
2023-04-05 16:10:10 -0500

    43.[Flux] - to Mono (Flux)

--- src/main/java/org/rp/sec02/helper/Lec09FluxFromMono.java
+++ src/main/java/org/rp/sec02/helper/Lec09FluxFromMono.java
@@ -6,9 +6,17 @@ import reactor.core.publisher.Mono;
 
 public class Lec09FluxFromMono {
     public static void main(String[] args) {
-        Mono<String> mono = Mono.just("a");
+        /*Mono<String> mono = Mono.just("a");
         Flux<String> flux = Flux.from(mono);
-        flux.subscribe(Util.onNext());
+        flux.subscribe(Util.onNext());*/
+        Flux.range(1,10)
+                .filter(i->i>3)
+                .next()
+                .subscribe(
+                Util.onNext(),
+                Util.onError(),
+                Util.onComplete()
+        );
     }
 
     private static void doSomething(Flux<String> flux){
