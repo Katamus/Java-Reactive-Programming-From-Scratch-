5c349c52608c842085c6c88a8860724e89f2c7b5
Christian <christiancatamuscay@r-fast.com>
2023-04-05 14:24:15 -0500

    35.[Flux] - From Array/List (Flux)

---
+++ src/main/java/org/rp/sec02/Lec03FluxFromArrayOrList.java
@@ -0,0 +1,18 @@
+package org.rp.sec02;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Lec03FluxFromArrayOrList {
+
+    public static void main(String[] args) {
+        //List<String> strings = Arrays.asList("a", "b", "c");
+        //Flux.fromIterable(strings).subscribe(Util.onNext());
+        Integer[] arr = {2,5,7,8};
+        Flux.fromArray(arr).subscribe(Util.onNext());
+    }
+
+}
