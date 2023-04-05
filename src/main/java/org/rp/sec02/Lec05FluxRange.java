7a87a43160d331120d79c2d890990861ad63e24c
Christian <christiancatamuscay@r-fast.com>
2023-04-05 14:45:29 -0500

    38.[Flux] - Log (Flux)

--- src/main/java/org/rp/sec02/Lec05FluxRange.java
+++ src/main/java/org/rp/sec02/Lec05FluxRange.java
@@ -6,7 +6,9 @@ import reactor.core.publisher.Flux;
 public class Lec05FluxRange {
     public static void main(String[] args) {
         Flux.range(3,10)
+                .log()
                 .map(integer -> Util.faker().name().fullName())
+                .log()
                 .subscribe(Util.onNext());
     }
 }
