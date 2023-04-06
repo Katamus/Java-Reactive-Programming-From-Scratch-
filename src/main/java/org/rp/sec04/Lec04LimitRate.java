2d9f73e5b836bd282afddef9577aec45214c6ba9
Christian <christiancatamuscay@r-fast.com>
2023-04-06 14:37:10 -0500

    70. Operator - Limit Rate (Operators)

---
+++ src/main/java/org/rp/sec04/Lec04LimitRate.java
@@ -0,0 +1,14 @@
+package org.rp.sec04;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec04LimitRate {
+
+    public static void main(String[] args) {
+        Flux.range(1,1000).log()
+                .limitRate(100,0)
+                .subscribe(Util.subscriber());
+    }
+
+}
