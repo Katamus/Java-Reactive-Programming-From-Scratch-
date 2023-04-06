5317155b60836f670dc616287f68b6f7bdd6e746
Christian <christiancatamuscay@r-fast.com>
2023-04-06 08:35:30 -0500

    66. Operator Handle - Until Condition (Operators)

---
+++ src/main/java/org/rp/sec04/Lec02HandleAssignment.java
@@ -0,0 +1,17 @@
+package org.rp.sec04;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec02HandleAssignment {
+    public static void main(String[] args) {
+        Flux.generate(synchronousSink -> synchronousSink.next(Util.faker().country().name()))
+                .map(Object::toString)
+                .handle((s, synchronousSink) -> {
+                   synchronousSink.next(s);
+                   if(s.toLowerCase().equals("canada"))
+                       synchronousSink.complete();
+                })
+                .subscribe(Util.subscriber());
+    }
+}
