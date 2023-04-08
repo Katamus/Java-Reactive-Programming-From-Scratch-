984e65a3fbbb219562c5d50ec29ae2c834ce9882
Christian <christiancatamuscay@r-fast.com>
2023-04-08 08:50:54 -0500

    96. More On Schedulers (Threading & Schedulers)

---
+++ src/main/java/org/rp/sec06/Lec03SubscribeOnMultipleItems.java
@@ -0,0 +1,40 @@
+package org.rp.sec06;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec03SubscribeOnMultipleItems {
+
+    public static void main(String[] args){
+        Flux<Object> flux = Flux.create(fluxSink -> {
+                    printThreadName("create");
+                    for (int i = 0; i < 4; i++) {
+                        fluxSink.next(i);
+                        Util.sleepSeconds(1);
+                    }
+
+                })
+                .doOnNext(i -> printThreadName("next " + i));
+
+       flux
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(v -> printThreadName("sub "+ v));
+
+        flux
+                .subscribeOn(Schedulers.parallel())
+                .subscribe(v -> printThreadName("sub "+ v));
+
+
+
+        Util.sleepSeconds(5);
+
+    }
+
+    private static void printThreadName(String msg){
+        System.out.println(msg+"\t\t:" + "thread : " + Thread.currentThread().getName());
+    }
+
+
+
+}
