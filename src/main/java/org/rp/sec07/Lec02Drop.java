8dde99f26316e0ecf64204e157331cbc8731376d
Christian <christiancatamuscay@r-fast.com>
2023-04-08 10:14:51 -0500

    108. OverFlow Strategy - Drop - Capturing Dropped Values (Backpressure/Overflow Strategy)

--- src/main/java/org/rp/sec07/Lec02Drop.java
+++ src/main/java/org/rp/sec07/Lec02Drop.java
@@ -4,11 +4,15 @@ import org.rp.courseutil.Util;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lec02Drop {
     public static void main(String[] args) {
 
         System.setProperty("reactor.bufferSize.small","16");
 
+        List<Object> list = new ArrayList<>();
         Flux.create(fluxSink -> {
                     for (int i = 0; i < 501; i++) {
                         fluxSink.next(i);
@@ -17,14 +21,15 @@ public class Lec02Drop {
                     }
                     fluxSink.complete();
                 })
-                .onBackpressureDrop()
+                .onBackpressureDrop(list::add)
                 .publishOn(Schedulers.boundedElastic())
                 .doOnNext(i->{
                     Util.sleepMillis(10);
                 })
                 .subscribe(Util.subscriber());
 
-        Util.sleepSeconds(60);
+        Util.sleepSeconds(10);
+        System.out.println(list);
 
     }
 }
