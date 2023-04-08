800deda3fa5e5f0311af95cad096f0a7dcc5ac1b
Christian <christiancatamuscay@r-fast.com>
2023-04-08 10:09:05 -0500

    107. Overflow Strategy - Drop - Part 2 (Backpressure/Overflow Strategy)

--- src/main/java/org/rp/sec07/Lec02Drop.java
+++ src/main/java/org/rp/sec07/Lec02Drop.java
@@ -6,10 +6,14 @@ import reactor.core.scheduler.Schedulers;
 
 public class Lec02Drop {
     public static void main(String[] args) {
+
+        System.setProperty("reactor.bufferSize.small","16");
+
         Flux.create(fluxSink -> {
                     for (int i = 0; i < 501; i++) {
                         fluxSink.next(i);
                         System.out.println("Push : "+i);
+                        Util.sleepMillis(1);
                     }
                     fluxSink.complete();
                 })
