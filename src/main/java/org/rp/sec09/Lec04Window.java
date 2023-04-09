d784623e622d4bff74af0d85c786af348ea7b901
Christian <christiancatamuscay@r-fast.com>
2023-04-08 20:49:37 -0500

    132. Batching With Windows - Demo (Batching)

---
+++ src/main/java/org/rp/sec09/Lec04Window.java
@@ -0,0 +1,38 @@
+package org.rp.sec09;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Lec04Window {
+
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+
+    public static void main(String[] args) {
+
+        eventStream()
+                .window(5)
+                .flatMap(flux -> saveEvents(flux))
+                .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(60);
+    }
+
+    private static Flux<String> eventStream(){
+        return Flux.interval(Duration.ofMillis(800))
+                .map(i -> "event "+i);
+    }
+
+    private static Mono<Integer> saveEvents(Flux<String> flux){
+        return flux.doOnNext(e-> System.out.println("Saving "+e))
+                .doOnComplete(()->{
+                    System.out.println("Saved this batch");
+                    System.out.println("----------------");
+                })
+                .then(Mono.just(atomicInteger.getAndIncrement()));
+    }
+
+}
