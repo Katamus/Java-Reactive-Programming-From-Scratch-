871cbece42dcc897012d6b63bd623654c8f86900
Christian <christiancatamuscay@r-fast.com>
2023-04-08 21:13:04 -0500

    135. Batching With Group By - Demo (Batching)

---
+++ src/main/java/org/rp/sec09/helper/Lec05Group.java
@@ -0,0 +1,24 @@
+package org.rp.sec09.helper;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Lec05Group {
+
+    public static void main(String[] args) {
+
+        Flux.range(1,30)
+                .delayElements(Duration.ofSeconds(1))
+                .groupBy(i -> i % 2) // Key 0 , 1
+                .subscribe(gf->process(gf,gf.key()));
+
+        Util.sleepSeconds(60);
+    }
+
+    private static void process(Flux<Integer> flux, int key){
+        System.out.println("Called");
+        flux.subscribe(i-> System.out.println("Key : "+key+",Item "+i));
+    }
+}
