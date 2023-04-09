9e080746ccc52ee90c22f2ea62a8b602f60fe8a7
Christian <christiancatamuscay@r-fast.com>
2023-04-09 09:01:15 -0500

    141. Repeat - Demo (Repeat & Retry)

---
+++ src/main/java/org/rp/sec10/Lec01Repeat.java
@@ -0,0 +1,25 @@
+package org.rp.sec10;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Flux;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Lec01Repeat {
+
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+
+    public static void main(String[] args) {
+        getIntegers()
+                .repeat(2)
+                .subscribe(Util.subscriber());
+    }
+
+    private static Flux<Integer> getIntegers(){
+        return Flux.range(1,3)
+                .doOnSubscribe(subscription -> System.out.println("Subcribed"))
+                .doOnComplete(() -> System.out.println("Completed"))
+                .map(i -> atomicInteger.getAndIncrement());
+    }
+
+}
