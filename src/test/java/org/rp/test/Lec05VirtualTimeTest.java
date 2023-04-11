1bf317fcc05c8b9a7b7b57eee4d4f18cb2936cd2
Christian <christiancatamuscay@r-fast.com>
2023-04-11 09:34:54 -0500

    175. Step Verifier - Virtual Time Test - Part 1 (Unit Testing With Step Verifier)

---
+++ src/test/java/org/rp/test/Lec05VirtualTimeTest.java
@@ -0,0 +1,28 @@
+package org.rp.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.rp.sec09.helper.BookOrder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class Lec05VirtualTimeTest {
+
+    @Test
+    public void test1() {
+        StepVerifier.withVirtualTime(()->timeConsumingFlux())
+                .thenAwait(Duration.ofSeconds(30))
+                .expectNext("1a","2a","3a","4a")
+                .verifyComplete();
+    }
+
+    private Flux<String> timeConsumingFlux(){
+        return Flux.range(1,4)
+                .delayElements(Duration.ofSeconds(5))
+                .map(i -> i+ "a");
+    }
+
+}
