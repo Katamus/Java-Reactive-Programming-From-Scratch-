a460f0813a3b3e394bf921cb8c059adcddfbb6f2
Christian <christiancatamuscay@r-fast.com>
2023-04-10 21:31:30 -0500

    166. Context - Demo - Part 1 (Context)

---
+++ src/main/java/org/rp/sec12/Lec01Ctx.java
@@ -0,0 +1,23 @@
+package org.rp.sec12;
+
+import org.rp.courseutil.Util;
+import reactor.core.publisher.Mono;
+import reactor.util.context.Context;
+
+public class Lec01Ctx {
+    public static void main(String[] args) {
+        getWelcomeMessage()
+        .contextWrite(Context.of("user","sam"))
+        .subscribe(Util.subscriber());
+    }
+
+    public static Mono<String> getWelcomeMessage(){
+        return Mono.deferContextual(contextView -> {
+            if(contextView.hasKey("user")){
+                return Mono.just("Welcome "+contextView.get("user"));
+            }else{
+                return Mono.error(new RuntimeException("unauthenticated"));
+            }
+        });
+    }
+}
